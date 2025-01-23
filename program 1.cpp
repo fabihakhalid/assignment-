4fea3fc70332fdec7810bbd27ff03303a043afea
fabiha khalid <fabihakhalid2005@gmail.com>
2025-01-22 19:13:43 -0800

    done

---
+++ program 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n,i=1,sum=0;
+	cout<<"enter your number :";
+	cin>>n;
+	while (i<=n){
+		cout<<i<<"*"<<i<<"="<<i*i<<endl;
+		sum+=i*i;
+		i++;
+		
+	}
+	cout<<"the result is "<<sum<<endl;
+	return sum;
+}
